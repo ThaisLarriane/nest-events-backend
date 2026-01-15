363464f18b07a2f53f4a2abae14a129951876118
Thais Larriane <thais.larriane@unesp.br>
2026-01-15 11:34:36 -0300

    responses and status codes

--- src/events.controller.ts
+++ src/events.controller.ts
@@ -1,19 +1,27 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from "@nestjs/common";
 
 @Controller('/events')
 export class EventsController{
     @Get()
-    findAll() {}
+    findAll() {
+        return[
+            {id: 1, name: 'First Event'},
+            {id: 1, name: 'Second Event'}
+        ];
+    }
     @Get(':id')
     findOne(@Param('id') id) {
-        return id;
+        return {id: 1, name: 'First Event'};
     }
     @Post()
     create(@Body() input) {
         return input;
     }
     @Patch(':id')
-    update(@Param('id') id, @Body() input) {}
-    @Delete()
-    remove() {}
+    update(@Param('id') id, @Body() input) {
+        return input;
+    }
+    @Delete(':id')
+    @HttpCode(204)
+    remove(@Param('id') id) {}
 }
