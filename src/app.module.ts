f00deb6645dfb0f9763e299877661ce5efbe5de7
Thais Larriane <thais.larriane@unesp.br>
2026-01-28 10:24:48 -0300

    connecting database with docker and config TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
+import { Event } from './event-entity';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsController } from './events.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: '127.0.0.1',
+    port: 3306,
+    username: 'root',
+    password: 'example', 
+    database: 'nest-events',
+    driver: require('mysql2'),
+    entities: [Event],
+    synchronize: true
+  })],
   controllers: [AppController, EventsController],
   providers: [AppService],
 })
